b3817926f70b317a099f03974d93fead54dcd40e
Chankaseam Thanaratkitjakarn xD <kzxp@msn.com>
2020-09-16 01:18:03 +0700

    Generate Al3 to Al2.

--- scripts/generate.js
+++ scripts/generate.js
@@ -12,4 +12,16 @@ const mapNationalities = () => {
   fs.writeFileSync('./nationalities.json', JSON.stringify(result))
 }
 
+const mapAlpha3ToAlpha2 = () => {
+  const result = countries.reduce((acc, value) => {
+    return {
+      ...acc,
+      [value.alpha_3_code]: value.alpha_2_code,
+    }
+  }, {})
+
+  fs.writeFileSync('./alpha_3_to_alpha_2.json', JSON.stringify(result))
+}
+
 mapNationalities()
+mapAlpha3ToAlpha2()
